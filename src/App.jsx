c5e5494b47eb1042c3406dee0c3621728b6471a7
10sandeep <sandeepnayak1424@gmail.com>
2025-01-22 15:12:51 +0530

    useState react hooks with example

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./App.css";
+
+//USESTATE
+
+// function App() {
+//   const [food, setfood] = useState("Chicken Biriyani");
+
+//   const changeFood = () => {
+//     setfood("Mutton Biriyani");
+//   };
+//   return (
+//     <>
+//       <h1>My Favourite Food is {food}</h1>
+//       <button onClick={changeFood}>Mutton Biriyani</button>
+//     </>
+//   );
+// }
+function App() {
+ const [count,setCount] =useState(0);
+
+ const incrementCount = () => {
+ setCount(count+4)
+// setCount(prev => prev+1)
+// setCount(prev => prev+1)
+// setCount(prev => prev+1)
+// setCount(prev => prev+1)
+ }
+ return(
+<>
+  <h1>Count : {count}</h1>
+  <button onClick={incrementCount}>Increment</button>
+</>
+
+ )
+}
+
+export default App;
